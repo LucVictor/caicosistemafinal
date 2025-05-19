761884d5c5240e453b7007504a507bfb1c2e2720
Lucas Victor <lucasvictor999@Outlook.com.br>
2025-05-19 10:59:16 -0300

    hot fix

--- app/routes/recebimento.py
+++ app/routes/recebimento.py
@@ -6,8 +6,18 @@ from ..models.models import Recebimento
 @app.route('/recebimento/', methods=['GET'])
 @login_required
 def index_recebimento():
-    recebimentos = Recebimento.query.all()
-    return render_template('recebimento/index.html', recebimentos=recebimentos)
+    recebimentos = Recebimento.query.filter(Recebimento.data_recebimento >= primeiro_dia_mes(), Recebimento.data_recebimento <= ultimo_dia_mes()).order_by(
+        Recebimento.data_recebimento.desc()).all()
+    total_recebimento = len(recebimentos)
+    recebimento_por_conferente = (db.session.query(Recebimento.conferente, func.count().label('quantidade'))
+    .filter(
+        Recebimento.data_recebimento >= primeiro_dia_mes(),
+        Recebimento.data_recebimento <= ultimo_dia_mes(),
+    )
+    .group_by(Recebimento.conferente)
+    .order_by(func.count().desc())  # opcional, ordena do maior pro menor
+    .all())
+    return render_template('recebimento/index.html', recebimento_por_conferente=recebimento_por_conferente, total_recebimento=total_recebimento, recebimentos=recebimentos)
 
 @app.route('/recebimento/cadastrar', methods=['GET', 'POST'])
 @login_required
